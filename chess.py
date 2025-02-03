833e6baab7e08ddd5de0243a1a4af0b7d33c5810
MISHA-414 <misha1.petruhin@gmail.com>
2025-02-03 13:12:02 +0700

    kubick

--- chess.py
+++ chess.py
@@ -3,3 +3,63 @@
 # Укажите координаты клетки, где находится ферзь, например, [4,4].
 # Определите клетки, которые атакует ферзь (в строке, столбце и диагоналях).
 # Визуализация: Используйте тепловую карту (imshow), чтобы показать шахматную доску. Отметьте положение ферзя и атакуемые клетки цветами.
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.matrixlib.defmatrix import matrix
+desk=[]
+for i in range(8):
+    if i%2==0:
+        desk.append([3 if i%2==0 else 0 for i in range(8)])
+    else:
+        desk.append([0 if i%2==0 else 3 for i in range(8)])
+# Здесь находиться ферзь
+x=int(input())
+y=8-int(input())
+color=1 # Чёрный
+desk[x][y]=1
+if x+y%2==0:   # Определяем цвет
+    color=2 # Белый
+    desk[x][y]=2
+
+# Красим диагонали
+up_x, up_y=x+1, y+1
+while up_x!=8 and up_y!=8:
+    desk[up_x][up_y]=color
+    up_x+=1
+    up_y+=1
+down_x, down_y=x-1, y-1
+while down_x!= -1 and down_y-y!= -1:
+    desk[down_x][down_y]=color
+    down_x-=1
+    down_y-=1
+left_x, left_y=x-1, y+1
+while left_x-x!= -1 and left_y-y!=8:
+    desk[left_x][left_y]=color
+    left_x-=1
+    left_y+=1
+right_x, right_y=x+1, y-1
+while right_x-x!=8 and right_y-y!= -1:
+    desk[right_x][right_y]=color
+    right_x+=1
+    right_y-=1
+# Вертикаль и горизонталь
+if x%2==0:
+    desk[x]=[1, 3, 1, 3, 1, 3, 1, 3]
+else:
+    desk[x] = [3, 1, 3, 1, 3, 1, 3, 1]
+
+
+
+
+plt.imshow(desk, cmap='gray')
+plt.xticks([0, 1, 2, 3, 4, 5, 6, 7], ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'])
+plt.yticks([0, 1, 2, 3, 4, 5, 6, 7], [1, 2, 3, 4, 5, 6, 7, 8])
+# Добавление цветовой шкалы
+plt.colorbar()
+plt.title("Тепловая карта")
+plt.show()
+
+
+
+
--- kubick.py
+++ kubick.py
@@ -5,3 +5,23 @@
 # Вероятность выпадения каждого значения.
 # Максимальное количество подряд выпавших одинаковых значений.
 # Визуализируйте результаты в виде гистограммы.
+import random
+import matplotlib.pyplot as plt
+lst=[random.randint(1, 6) for i in range(1001)]
+lst_count=[lst.count(1), lst.count(2), lst.count(3), lst.count(4), lst.count(5), lst.count(6)]
+plt.bar([i for i in range(1, 7)], lst_count, color='purple')
+for i in range(1, 7):
+    plt.text(i, lst_count[i-1], lst_count[i-1]/1000)
+plt.title("Столбчатая диаграмма")
+plt.show()
+# Ищем максимальное количество подряд выпавших одинаковых значений
+max_len=0
+line=1
+for i in range(1000):
+    if lst[i]==lst[i+1]:
+        line+=1
+    else:
+        if line>max_len:
+            max_len=line
+        line=1
+print(max_len)
