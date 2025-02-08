54e55ccc2f8a5e2e893bc258da55ca4029a28503
MISHA-414 <misha1.petruhin@gmail.com>
2025-02-08 11:23:58 +0700

    weather

--- wether.py
+++ wether.py
@@ -11,70 +11,54 @@
 
 import random
 import matplotlib.pyplot as plt
-import requests
-import json
-
-# Прогноз погоды
-#def weather_answer(city):
-city='Novosibirsk'
-url = ('https://api.weather.yandex.ru/v2/forecast' )
 
 
-# Задаем координаты населенного пункта
-lat = 55.75396 # широта Москвы
-lon = 37.620393 # долгота Москвы
-
-# Задаем параметры запроса
-params = {
-    'lat': lat,
-    'lon': lon,
-    'lang': 'ru_RU', # язык ответа
-    'limit': 7, # срок прогноза в днях
-    'hours': True, # наличие почасового прогноза
-    'extra': False # подробный прогноз осадков
-}
-
-# Задаем значение ключа API
-api_key = '041533d7-d5c5-42ab-aca2-6627db4b69d3'
-
-# Задаем URL API
-url = 'https://api.weather.yandex.ru/v2/forecast'
-
-# Делаем запрос к API
-response = requests.get(url, params=params, headers={'X-Yandex-API-Key': api_key})
-print(response)
-'''weather_data = requests.get(url).json()
-weather_data_structure = json.dumps(weather_data, indent=2)
-'''
-'''temperature = weather_data['main']['temp']
-feels_like = weather_data['main']['feels_like']
-wind = weather_data['wind']['speed']'''
-
-    #return f'Температура {round(temperature)} градусов, ощущаеться как {round(feels_like)} градусов. Скорость ветра {wind} м/с'
-
-
-
-'''
 
 temp=[random.randint(-10, 36) for i in range(365)]
 average_temp=sum(temp)/365
 higher_25=0
 max_len = 0
 line = 1
+hot=[]
+cold=[]
 for i in range(365):
     if temp[i]>25:
         higher_25+=1
 
     if temp[i] < 0:
         line += 1
+        cold.append(i)
     else:
+        hot.append(i)
         if line > max_len:
             max_len = line
         line = 1
 print('Подряд ниже 0:', max_len)
 print('Выше 25:', higher_25)
 
-plt.plot(temp, color='blue')
-plt.title("Погода")
+# Создание подграфиков
+fig, axs = plt.subplots(1, 2, figsize=(10, 4))  # 1 ряд, 2 столбца
+
+# График 1
+
+axs[0].scatter(hot, [temp[i] for i in hot], color="red")
+axs[0].scatter(cold, [temp[i] for i in cold], color="blue")
+axs[0].plot([i for i in range(365)], temp, color='grey')
+axs[0].set_title("Погода")
+axs[0].set_ylabel("Температура")
+axs[0].set_xlabel("День")
+
+
+
+count_day=[0]*45
+for i in temp:
+    count_day[i-10]+=1
+
+# График 2
+axs[1].bar([i for i in range(-10, 35)], count_day, color="green")
+axs[1].set_title("Распределение Погоды")
+axs[1].set_ylabel("Кол-во дней")
+axs[1].set_xlabel("Температура")
+
 plt.show()
-'''
+
